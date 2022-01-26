af4ea74202a573495cc286bcd41223b56c516797
umar100 <aumar1665@gmail.com>
2022-01-26 01:33:22 +0100

    first commit

--- src/com/megapinions/Main.java
+++ src/com/megapinions/Main.java
@@ -1,8 +1,15 @@
 package com.megapinions;
 
+import printer.Printer;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Printer printer = new Printer(20, true);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed = " + pagesPrinted + " and new total print count for print = " + printer.getPagesPrinted());
+        pagesPrinted=printer.printPages(2);
+        System.out.println("Pages printed = " + pagesPrinted + " and new total print count for print = " + printer.getPagesPrinted());
     }
 }
